7e8a88e088a5580f3f491e2bdaf5b5a7b365bfb8
staduru <staduru@iu.edu>
2016-05-13 00:42:26 -0400

    clean up

--- src/big-data-stack-chameleon/roles/nagios_server/runtest.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -xe
-
-name=$(basename $PWD)
-sudo docker build -t $name .
-sudo docker run --rm -t -i $name
--- src/launch.sh
+++ src/launch.sh
@@ -1,16 +1,14 @@
-#nova delete $USER-master0 $USER-master1 $USER-master2
-#sleep 60s
 mypwd=$(pwd)
-cd "big-data-stack-chameleon/"
+cd "./../../big-data-stack/"
 vcl boot -p openstack -P $USER-
 sleep 200s
 ansible-playbook play-hadoop.yml
 ansible-playbook addons/pig.yml
 cd ${mypwd}
-rm -rf "big-data-stack-chameleon/examples/src"
-mkdir "big-data-stack-chameleon/examples/src"
-/bin/bash -c "cp -avr pig.benchmarking.i590/src/* big-data-stack-chameleon/examples/src/"
-cd "big-data-stack-chameleon/"
-ls -al
+rm -rf "../../big-data-stack/examples/src"
+mkdir  "../../big-data-stack/examples/src"
+/bin/bash -c "cp -avr pig.benchmarking.i590/src/* ../../big-data-stack/examples/src/"
+cd "./../../big-data-stack/"
 ansible-playbook examples/src/site.yml
 nova list | grep $USER-
+cd ${mypwd}
